8618ef0c0a67aa24cdd309b94b2109ffaa336249
Josh C <legomania@cox.net>
2016-01-10 09:49:07 -0800

    Initial Commit

---
+++ War game tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  War game tutorial
+//
+//  Created by Josh Christensen on 1/10/16.
+//  Copyright © 2016 OrangeDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
